6ef54bb2ecea44e7ffcc102e2606459f931fa563
Junyoung Park <wnsdud761@gmail.com>
2024-10-06 05:28:04 +0900

    Feat: 헤더 스타일

--- src/app/ui/header-nav.tsx
+++ src/app/ui/header-nav.tsx
@@ -2,11 +2,17 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Noto_Sans_KR } from "next/font/google";
 
+const notoSans = Noto_Sans_KR({ subsets: ["latin"] });
 const links = [
   {
-    name: '웹툰/만화',
+    name: '웹툰',
     href: '/webtoon'
+  },
+  {
+    name: '만화',
+    href: '/comics  '
   },
   {
     name: '웹소설',
@@ -29,8 +35,8 @@ export const HeaderNav = () => {
     <ul className="flex">
       { links.map((link) => {
         return (
-          <li key={`header-nav-${link.name}`} className={` p-3`}>
-            <Link href={link.href}><p className={`${pathname === link.href ? 'text-black' : 'text-[#aaa]'}`}>{link.name}</p></Link>
+          <li key={`header-nav-${link.name}`} className='p-3'>
+            <Link href={link.href}><p className={`header-nav text-sm font-bold ${pathname === link.href ? 'text-[#141414] cursor-default' : 'text-[#787878] hover:opacity-40'} ${notoSans.className}`}>{link.name}</p></Link>
           </li>
         )
       })}
